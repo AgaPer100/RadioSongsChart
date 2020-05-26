3c8e893ffd4bcffe658565e022ee9dd443119746
Agnieszka Perkowska <agaper100@gmail.com>
2020-05-27 00:12:07 +0200

    rendered data to the browser

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ if(!$conn){
 }
 
 //write query for all songs
-$sql = 'SELECT nickname, songs, id FROM songs';
+$sql = 'SELECT nickname, songs, id FROM songs ORDER BY created_at';
 
 //make query & get result
 $result = mysqli_query($conn, $sql);
 
 //fetch the resulting rows as an array
-$songs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$charts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 //free result from memory
 mysqli_free_result($result);
@@ -23,7 +23,7 @@ mysqli_free_result($result);
 //close connection
 mysqli_close($conn);
 
-print_r($songs);
+//explode(',', $charts[0]['songs']);
 
 
 ?>
@@ -32,6 +32,32 @@ print_r($songs);
 <html lang="en">
 
 <?php include('templates/header.php') ?>
+
+<h4 class="center grey-text">Hits!</h4>
+
+<div class="container">
+    <div class="row">
+        <?php foreach($charts as $chart){ ?>
+
+            <div class="col s6 md3">
+                <div class="card">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($chart['nickname']); ?></h6>
+                        <ul>
+                            <?php foreach(explode(',', $chart['songs']) as $sng){ ?>
+                                <li><?php echo htmlspecialchars($sng); ?></li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
+    </div>
+</div>
 
 <?php include('templates/footer.php') ?>
     
