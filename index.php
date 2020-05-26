bf26a689c01ab5f99bd7852a6dd121e5f85c6c78
Agnieszka Perkowska <agaper100@gmail.com>
2020-05-26 23:45:05 +0200

    got data from database

--- index.php
+++ index.php
@@ -8,6 +8,24 @@ if(!$conn){
     echo 'Connection error: ' . mysqli_connect_error();
 }
 
+//write query for all songs
+$sql = 'SELECT nickname, songs, id FROM songs';
+
+//make query & get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$songs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($conn);
+
+print_r($songs);
+
+
 ?>
 
 <!DOCTYPE html>
