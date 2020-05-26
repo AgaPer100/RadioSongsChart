ab8cfa9127dd0deeb85255dcaf4ef0478edbce72
Agnieszka Perkowska <agaper100@gmail.com>
2020-05-26 14:32:18 +0200

    added validations

--- add.php
+++ add.php
@@ -1,9 +1,36 @@
 <?php
 
+$nickname = $email = $songs = '';
+$errors = ['email'=>'', 'nickname'=>'', 'songs'=>''];
+
 if(isset($_POST['submit'])){
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['nickname']);
-    echo htmlspecialchars($_POST['songs']);
+
+    //check
+    if(empty($_POST['email'])){
+        $errors['email'] = 'An email is required <br/>';
+    } else {
+        $email = $_POST['email'];
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors['email'] = 'Email must be a valid email address';
+        }
+    }
+    if(empty($_POST['nickname'])){
+        $errors['nickname'] = 'A nickname is required <br/>';
+    } else {
+        $nickname = $_POST['nickname'];
+        if(!preg_match('/^[a-zA-Z\s]+$/', $nickname)){
+            $errors['nickname'] = 'Nickname must be letters and spaces only';
+        }
+    }
+    if(empty($_POST['songs'])){
+        $errors['songs'] = 'Enter 3 songs <br/>';
+    } else {
+        $songs = $_POST['songs'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $songs)){
+            $errors['songs'] = 'Songs must be a comma separated list';
+        }
+    }
+
 }
 
 ?>
@@ -17,11 +44,14 @@ if(isset($_POST['submit'])){
     <h4 class="center">Add your favorite songs</h4>
     <form action="add.php" method="POST" class="white">
         <label>Your Email:</label>
-        <input type="email" name="email">
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text"><?php echo $errors['email'] ?></div>
         <label>Your Nickname:</label>
-        <input type="text" name="nickname">
+        <input type="text" name="nickname" value="<?php echo htmlspecialchars($nickname) ?>">
+        <div class="red-text"><?php echo $errors['nickname'] ?></div>
         <label>3 favorite songs (comma separated):</label>
-        <input type="text" name="songs">
+        <input type="text" name="songs" value="<?php echo htmlspecialchars($songs) ?>">
+        <div class="red-text"><?php echo $errors['songs'] ?></div>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand">
         </div>
