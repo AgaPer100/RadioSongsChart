ac71e31f829b1c2832c822f06a53c9963182b56d
Agnieszka Perkowska <agaper100@gmail.com>
2020-05-27 12:32:59 +0200

    added delete form

--- details.php
+++ details.php
@@ -2,6 +2,22 @@
 
 include('config/db_connect.php');
 
+if(isset($_POST['delete'])){
+
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM songs WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+        //success
+        header('Location: index.php');
+    } {
+        //failure
+        echo 'query error: ' . mysqli_error($conn);
+    }
+
+}
+
 //check GET request id param
 if(isset($_GET['id'])){
 
@@ -36,6 +52,12 @@ if(isset($_GET['id'])){
             <p><?php echo date($song['created_at']); ?></p>
             <h5>Songs: </h5>
             <p><?php echo htmlspecialchars($song['songs']); ?></p>
+
+            <!-- Delete form -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $song['id'] ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand">
+            </form>
 
         <?php else: ?>
 
